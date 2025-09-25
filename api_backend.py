871a08d34accc4d0da2f450588b1d430d70591c8
1Croreproject <1cp.python02@gmail.com>
2025-09-25 04:38:39 -0700

    Update api_backend.py

--- api_backend.py
+++ api_backend.py
@@ -2070,4 +2070,10 @@ async def convert(
 
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
-    
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Fallback to 8000 locally
+    uvicorn.run("api_backend:app", host="0.0.0.0", port=port)
+
